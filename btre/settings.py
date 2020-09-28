8711d5064beafe98732fcabe35f784833101530a
Adrian Barros <adriansbarros1@gmail.com>
2020-09-27 23:09:13 -0400

    delete old database

--- btre/settings.py
+++ btre/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 #  need to configure the database to use postgres
+# old data base deleted
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
